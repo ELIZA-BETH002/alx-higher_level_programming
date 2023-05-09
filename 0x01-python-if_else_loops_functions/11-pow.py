8de4d7ef73e99a0e3afe74bd4a681fffdf07ccf1
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-05-09 14:34:00 +0100

    Power

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def pow(a, b):
     """Return the value of a raised to the power of b"""
-    result = 1
-    for _ in range(b):
-        result *= a
-    return result
-
+    result
