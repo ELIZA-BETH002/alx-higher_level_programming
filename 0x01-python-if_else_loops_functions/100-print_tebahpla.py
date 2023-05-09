e5f2817a4342af8581014c2b4ac6a0579bc4c57a
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-05-09 16:00:56 +0100

    ReverseAlphabet

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,4 +3,4 @@ for i in range(ord('z'), ord('a')-1, -1):
     if (i - ord('z')) % 2 == 0:
         print("{}".format(chr(i).lower()), end='')
     else:
-        print(f"{}".format(chr(i).upper()), end='')
+        print("{}".format(chr(i).upper()), end='')
