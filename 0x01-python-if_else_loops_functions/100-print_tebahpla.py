adb58afcbd1b7f6116e09dcaf20ad2abefeec89c
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-05-09 16:00:06 +0100

    Teb alpha

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('a')-1, -1):
     if (i - ord('z')) % 2 == 0:
-        print(f"{}".format(chr(i).lower()), end='')
+        print("{}".format(chr(i).lower()), end='')
     else:
         print(f"{}".format(chr(i).upper()), end='')
