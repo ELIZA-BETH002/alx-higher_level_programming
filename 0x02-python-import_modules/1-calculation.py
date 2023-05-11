02986e633d1385cf7b0ec98176d980c90c8f6e67
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-05-11 15:51:51 +0100

    Calc.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-# Assign values to a and b
-a = 10
-b = 5
 
-# Import specific functions from calculator_1.py
-from calculator_1 import add, subtract, multiply, divide
+    from calculator_1 import add, sub, mul, div
 
-# Perform mathematical operations
-sum_result = add(a, b)
-diff_result = subtract(a, b)
-prod_result = multiply(a, b)
-div_result = divide(a, b)
+    a = 10
 
-# Print the results
-print("Sum: {}".format(sum_result))
-print("Difference: {}".format(diff_result))
-print("Product: {}".format(prod_result))
-print("Division: {}".format(div_result))
+    b = 5
+
+    print('{:d} + {:d} = {:d}'.format(a, b, (add(a, b))))
+    
+    print('{:d} - {:d} = {:d}'.format(a, b, (sub(a, b))))
+
+    print('{:d} * {:d} = {:d}'.format(a, b, (mul(a, b))))
+
+    print('{:d} / {:d} = {:d}'.format(a, b, (div(a, b))))
