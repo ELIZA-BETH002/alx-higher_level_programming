78e85a38f31f6e2123459ceca2ee182abf95dca1
ELIZA-BETH002 <elizabethnwaugo@gmail.com>
2023-05-11 07:31:30 -0700

    infinite

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-import sys
-
-# Get the command-line arguments
-arguments = sys.argv[1:]  # Exclude the script name
-
-# Calculate the sum of the arguments
-result = sum(int(arg) for arg in arguments)
-
-# Print the result
-print(result)
+from sys import argv
+add = 0
+for s in argv[1:]:
+    add += int(s)
+print("{:d}".format(add))
