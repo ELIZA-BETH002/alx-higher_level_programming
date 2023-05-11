8693b41e8c9bb456956f11f759423692e24e6f5c
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-05-11 15:44:26 +0100

    add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-# Assign values to a and b
+if __name__ == "__main__":
+  # Assign values to a and b
+
 a = 1
+
 b = 2
 
 # Import the add function from add_0.py
+
 import add_0
 
 # Calculate the result using the add function
+
 result = add_0.add(a, b)
 
 # Print the formatted output
+
 print("{} + {} = {}".format(a, b, result))
