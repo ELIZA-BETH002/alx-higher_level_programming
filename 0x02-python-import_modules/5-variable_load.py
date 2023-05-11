c9295fe5a3bf38e50a5d0fdb6bb9a349b3d63848
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-05-11 15:38:25 +0100

    Variable load

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-# Import the variable a from variable_load_5.py
-from variable_load_5 import a
+if __name__ == "__main__":
+    # Import the variable a from variable_load_5.py
+    from variable_load_5 import a
 
-# Print the value of a
-print(a)
+    # Print the value of a
+    print(a)
