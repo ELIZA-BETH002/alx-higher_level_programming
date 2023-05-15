7153e3a7e76965d71f998bc460315b26bbc721cd
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-05-15 08:52:28 +0100

    Palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,85 +1,125 @@
-#include <lists.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include "lists.h"
 
-typedef struct listint_s {
-    int data;
-    struct listint_s *next;
-} listint_t;
 
-int is_palindrome(listint_t **head) {
-    if (*head == NULL || (*head)->next == NULL) {
-        return 1; // Empty list or single node list is considered a palindrome
+
+int is_palindrome(listint_t **head)
+
+{
+
+    listint_t *start = NULL, *end = NULL;
+
+    unsigned int i = 0, len = 0, len_cyc = 0, len_list = 0;
+
+    if (head == NULL)
+
+        return (0);
+
+    if (*head == NULL)
+
+        return (1);
+
+    
+
+    start = *head;
+
+    len = listint_len(start);
+
+    len_cyc = len * 2;
+
+    len_list = len_cyc - 2;
+
+    end = *head;
+
+    for (; i < len_cyc; i = i + 2)
+
+    {
+
+        if (start[i].n != end[len_list].n)
+
+            return (0);
+
+        len_list = len_list - 2;
+
     }
 
-    // Find the middle of the linked list
-    listint_t *slow = *head;
-    listint_t *fast = *head;
-    while (fast->next != NULL && fast->next->next != NULL) {
-        slow = slow->next;
-        fast = fast->next->next;
-    }
+    return (1);
 
-    // Reverse the second half of the linked list
-    listint_t *prev = NULL;
-    listint_t *curr = slow->next;
-    while (curr != NULL) {
-        listint_t *next = curr->next;
-        curr->next = prev;
-        prev = curr;
-        curr = next;
-    }
-
-    // Compare the first half with the reversed second half
-    listint_t *first = *head;
-    listint_t *second = prev;
-    while (second != NULL) {
-        if (first->data != second->data) {
-            return 0; // Not a palindrome
-        }
-        first = first->next;
-        second = second->next;
-    }
-
-    return 1; // Palindrome
 }
 
-int main() {
-    // Create a sample linked list
-    listint_t *head = NULL;
-    listint_t *node1 = malloc(sizeof(listint_t));
-    listint_t *node2 = malloc(sizeof(listint_t));
-    listint_t *node3 = malloc(sizeof(listint_t));
-    listint_t *node4 = malloc(sizeof(listint_t));
-    listint_t *node5 = malloc(sizeof(listint_t));
-    
-    node1->data = 1;
-    node1->next = node2;
-    node2->data = 2;
-    node2->next = node3;
-    node3->data = 3;
-    node3->next = node4;
-    node4->data = 2;
-    node4->next = node5;
-    node5->data = 1;
-    node5->next = NULL;
+/**
 
-    head = node1;
+  * get_nodeint_at_index - Gets a node from a linked list
 
-    // Check if the linked list is a palindrome
-    int result = is_palindrome(&head);
-    if (result == 1) {
-        printf("The linked list is a palindrome.\n");
-    } else {
-        printf("The linked list is not a palindrome.\n");
-    }
+  * @head: The head of the linked list
 
-    // Clean up the memory
-    while (head != NULL) {
-        listint_t *temp = head;
-        head = head->next;
-        free(temp);
-    }
+  * @index: The index to find in the linked list
 
-    return 0;
+  *
+
+  * Return: The specific node of the linked list
+
+  */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+
+{
+
+	listint_t *current = head;
+
+	unsigned int iter_times = 0;
+
+	if (head)
+
+	{
+
+		while (current != NULL)
+
+		{
+
+			if (iter_times == index)
+
+				return (current);
+
+			current = current->next;
+
+			++iter_times;
+
+		}
+
+	}
+
+	return (NULL);
+
+}
+
+/**
+
+  * slistint_len - Counts the number of elements in a linked list
+
+  * @h: The linked list to count
+
+  *
+
+  * Return: Number of elements in the linked list
+
+  */
+
+size_t listint_len(const listint_t *h)
+
+{
+
+	int lenght = 0;
+
+	while (h != NULL)
+
+	{
+
+		++lenght;
+
+		h = h->next;
+
+	}
+
+	return (lenght);
+
 }
