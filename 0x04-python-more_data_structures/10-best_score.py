6e137a40c42255927b68417dbbe7979621e23dcc
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-05-17 18:11:42 +0100

    Best

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,11 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary is None or len(a_dictionary) == 0:
         return None
-    
     max_value = max(a_dictionary.values())
-
     for best_k, v in a_dictionary.items():
-
         if max_value == v:
-
             return best_k
