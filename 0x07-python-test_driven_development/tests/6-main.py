876086af4cb7962a59c4ca76d233b544ccf9813f
ELIZA-BETH002 <elizabethnwaugo@gmail.com>
2023-06-02 11:17:04 -0700

    all

---
+++ 0x07-python-test_driven_development/tests/6-main.py
@@ -0,0 +1,7 @@
+from max_integer import max_integer
+
+# Example usage of the max_integer function
+numbers = [1, 2, 3, 4]
+result = max_integer(numbers)
+print(f"The maximum integer in {numbers} is: {result}")
+
---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+from max_integer import max_integer
+
+class TestMaxInteger(unittest.TestCase):
+
+    def test_empty_list(self):
+        """Test when the list is empty"""
+        result = max_integer([])
+        self.assertIsNone(result)
+
+    def test_single_element(self):
+        """Test when the list contains a single element"""
+        result = max_integer([1])
+        self.assertEqual(result, 1)
+
+    def test_positive_numbers(self):
+        """Test when the list contains positive numbers"""
+        result = max_integer([1, 2, 3, 4])
+        self.assertEqual(result, 4)
+
+    def test_negative_numbers(self):
+        """Test when the list contains negative numbers"""
+        result = max_integer([-1, -2, -3, -4])
+        self.assertEqual(result, -1)
+
+    def test_mixed_numbers(self):
+        """Test when the list contains mixed positive and negative numbers"""
+        result = max_integer([-1, 2, -3, 4])
+        self.assertEqual(result, 4)
+
+    def test_duplicate_numbers(self):
+        """Test when the list contains duplicate numbers"""
+        result = max_integer([1, 2, 2, 4])
+        self.assertEqual(result, 4)
+
+    def test_large_numbers(self):
+        """Test when the list contains large numbers"""
+        result = max_integer([10**9, 10**10, 10**11])
+        self.assertEqual(result, 10**11)
+
+if __name__ == __main__:
+    unittest.main()
