849c15e77a86f490e817efd1da68572f35ddb137
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-06-10 23:01:39 +0100

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
+
 """
-contains the MyList class
+
+contains MyList class
+
 """
+
+
 class MyList(list):
+
+    """a subclass of list"""
+
+    def __init__(self):
+
+        """initializes the object"""
+
+        super().__init__()
+
     def print_sorted(self):
-        sorted_list = sorted(self)
+
         """prints the sorted list"""
-        print(sorted_list)
+
+        print(sorted(self))
