238a14c33d3ab097f9c658fa3ebeae755f7cb7ca
ELIZA-BETH002 <elizabethnwaugo@gmail.com>
2023-06-13 07:15:27 +0000

    input output

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This function appends a string
+"""
+
+def append_write(filename="", text=""):
+    """Returns number of characters added:"""
+    with open(filename, a, encoding=utf=8) as f:
+        return f.write(text)
