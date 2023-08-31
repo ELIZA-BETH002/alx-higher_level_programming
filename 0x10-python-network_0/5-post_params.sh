2e0f55ff33181fe11c5c62c836e6ededec7f3839
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-08-31 18:07:12 +0100

    param

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# This sends a POST request to the passed URL using curl, and then displays the body of the response
+
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
