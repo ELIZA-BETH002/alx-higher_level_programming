6a90cf75b55c6fe56b335493cab88a8cb75df18c
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-08-31 18:29:52 +0100

    body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 # This script sends a GET request to an URL with curl, and display the body of the response
 
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
+url=$1
 
-# Send a GET request to the provided URL and store the response
-response=$(curl -s -w "%{http_code}" "$1")
+# Send a GET request to the URL
+response=$(curl -s -X GET "$url")
 
-# Check if the response status code is 200
-if [ "$response" == "200" ]; then
-    # Display only the body of the response
-    curl -s "$1"
+# Check the status code of the response
+status_code=$(echo "$response" | head -1 | awk '{print $2}')
+
+# Only display the body of the response if the status code is 200
+if [ "$status_code" == "200" ]; then
+  echo "$response" | tail -n +2
 fi
